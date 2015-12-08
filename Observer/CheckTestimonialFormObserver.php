d0c756aebb687252a0bccee4734d6c61164c974d
Roman Chumak <chumakrom@gmail.com>
2015-12-08 11:53:57 +0200

    Optional captcha added on new testimonial form

---
+++ Observer/CheckTestimonialFormObserver.php
@@ -0,0 +1,84 @@
+<?php
+namespace Swissup\Testimonials\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Captcha\Observer\CaptchaStringResolver;
+
+class CheckTestimonialFormObserver implements ObserverInterface
+{
+    /**
+     * @var \Magento\Captcha\Helper\Data
+     */
+    protected $_helper;
+
+    /**
+     * @var \Magento\Framework\App\ActionFlag
+     */
+    protected $_actionFlag;
+
+    /**
+     * @var \Magento\Framework\Message\ManagerInterface
+     */
+    protected $messageManager;
+
+    /**
+     * @var \Magento\Framework\App\Response\RedirectInterface
+     */
+    protected $redirect;
+
+    /**
+     * @var CaptchaStringResolver
+     */
+    protected $captchaStringResolver;
+    /**
+     * Generic session
+     *
+     * @var \Magento\Framework\Session\Generic
+     */
+    protected $_testimonialSession;
+    /**
+     * @param \Magento\Captcha\Helper\Data $helper
+     * @param \Magento\Framework\App\ActionFlag $actionFlag
+     * @param \Magento\Framework\Message\ManagerInterface $messageManager
+     * @param \Magento\Framework\App\Response\RedirectInterface $redirect
+     * @param CaptchaStringResolver $captchaStringResolver
+     * @param \Magento\Framework\Session\Generic $testimonialSession
+     */
+    public function __construct(
+        \Magento\Captcha\Helper\Data $helper,
+        \Magento\Framework\App\ActionFlag $actionFlag,
+        \Magento\Framework\Message\ManagerInterface $messageManager,
+        \Magento\Framework\App\Response\RedirectInterface $redirect,
+        CaptchaStringResolver $captchaStringResolver,
+        \Magento\Framework\Session\Generic $testimonialSession
+    ) {
+        $this->_helper = $helper;
+        $this->_actionFlag = $actionFlag;
+        $this->messageManager = $messageManager;
+        $this->redirect = $redirect;
+        $this->captchaStringResolver = $captchaStringResolver;
+        $this->_testimonialSession = $testimonialSession;
+    }
+
+    /**
+     * Check CAPTCHA on New Testmonial page
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $formId = 'testimonial_new';
+        $captcha = $this->_helper->getCaptcha($formId);
+        if ($captcha->isRequired()) {
+            /** @var \Magento\Framework\App\Action\Action $controller */
+            $controller = $observer->getControllerAction();
+            if (!$captcha->isCorrect($this->captchaStringResolver->resolve($controller->getRequest(), $formId))) {
+                $this->_testimonialSession->setFormData($controller->getRequest()->getPost());
+                $this->messageManager->addError(__('Incorrect CAPTCHA.'));
+                $this->_actionFlag->set('', \Magento\Framework\App\Action\Action::FLAG_NO_DISPATCH, true);
+                $this->redirect->redirect($controller->getResponse(), 'testimonials/index/new');
+            }
+        }
+    }
+}
